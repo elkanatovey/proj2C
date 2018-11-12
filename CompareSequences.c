8c2bb01f0f8752a037da423f964be796420484f5
elkanatovey <elkanatovey@gmail.com>
2018-11-12 17:55:29 +0200

    continued

--- CompareSequences.c
+++ CompareSequences.c
@@ -22,13 +22,40 @@
 
 // my includes
 #define WRONG_NUMBER_OF_ARGS 4
-#define USAGE_ERROR "Usage: 
+#define USAGE_ERROR "Usage:   \n"
 #define FILE_NAME 1
 #define BAD_FILE_NAME_ERROR "Error opening file: %s\n"
+#define  MAX_LINE_LENGTH 100
+#define HEADER_START ">"
 
 
+typedef struct Sequence
+{
+    char *sequenceName;
+    int sequenceNameSize;
+    char *actualSequence;
+    int actualSequenceSize;
+}Sequence;
 
 
+int readLines(const FILE *currentFile, char *fileName)
+{
+    int i = 0;
+    Sequence *sequencesPointer = NULL;
+//    sequencesPointer =
+    char currentLine[MAX_LINE_LENGTH];
+    char *sequenceAppender = NULL;
+    while(fgets(currentLine, MAX_LINE_LENGTH, (FILE *) currentFile) != NULL)
+    {  //possible edge case empty line
+//        if(currentLine[0] == HEADER_START)
+//        {
+//            i++;
+//            sequencesPointer = (*sequencesPointer)realloc(sequencesPointer, sizeof(Sequence)*i);
+//            sequencesPointer[i-1].
+        printf("aaa");
+        }
+    }
+}
 
 
 
@@ -39,14 +66,14 @@ int main(int argc, char *argv[])
 {
 	if(argc != WRONG_NUMBER_OF_ARGS) // no files entered
 	{
-		printf(USAGE_ERROR);  //usage print and exits
+	    printf(USAGE_ERROR);  //usage print and exits
 		return 1;
 	}
 	FILE *currentFile;
-	currentFile = fopen(argv[FILENAME], "r");
+	currentFile = fopen(argv[FILE_NAME], "r");
 	if(currentFile == NULL)
 	{
-		fprintf(stderr, BAD_FILE_NAME_ERROR, argv[FILENAME]);
+		fprintf(stderr, BAD_FILE_NAME_ERROR, argv[FILE_NAME]);
 		return 1;
 	}
 	else
